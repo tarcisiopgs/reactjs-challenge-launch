2f87253da57d1b8da6b12770b37c6c499f5f04a4
Tarcísio Pedro <tarcisiopgs@gmail.com>
2019-05-15 13:21:44 -0300

    repo update

--- src/pages/Client.js
+++ src/pages/Client.js
@@ -1,108 +1,147 @@
-import React, {Fragment, useEffect, useState} from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
-import Header from "../components/Header";
-import { Form, Input, Scope } from "@rocketseat/unform";
+import Header from '../components/Header';
+import { Form, Input, Scope } from '@rocketseat/unform';
 import { Grid, Button, Typography } from '@material-ui/core';
 import api from '../services/api';
 
 const button = {
-    link: '/',
-    label: 'VOLTAR',
+  link: '/',
+  label: 'VOLTAR'
 };
-const title = 'Adicionar cliente';
+let title = 'Adicionar cliente';
 
-export default function Client({history, match}) {
-    const [data, setData] = useState({});
+export default function Client({ history, match }) {
+  const [data, setData] = useState({});
 
-    async function handleSubmit(data) {
-        await api.postOrPut('/clients', match.params.id, data);
+  async function handleSubmit(data) {
+    await api.postOrPut('/clients', match.params.id, data);
 
-        history.push('/');
+    history.push('/');
+  }
+
+  useEffect(() => {
+    async function loadData() {
+      const { id } = match.params;
+      const response = await api.get(`/clients/${id}`);
+
+      setData(response.data);
     }
 
-    useEffect(() => {
-       async function loadData() {
-           const {id} = match.params;
-           const response = await api.get(`/clients/${id}`);
+    if (match.params.id) {
+      title = 'Editar cliente';
+      loadData();
+    }
+  }, [match.params, match.params.id]);
 
-           setData(response.data);
-       }
-
-       if(match.params.id) {
-           loadData();
-       }
-    }, [match.params, match.params.id]);
-
-    return (
-        <Fragment>
-            <Header title={title} button={button}/>
-            <Form style={{maxWidth: 1000, margin: '0 auto', marginTop: 50}} onSubmit={handleSubmit} initialData={data} >
-            <Grid container justify="center" spacing={16}>
-                <Grid item xs={12}>
-                    <Typography color="textSecondary" variant="caption">
-                        Autenticação
-                    </Typography>
-                </Grid>
-                    <Scope path="authentication">
-                        <Grid item xs={12}>
-                            <Input required label="E-mail" name="email" type="email" />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Input required label="Senha" name="password" type="password" />
-                        </Grid>
-                    </Scope>
-                <Grid item xs={12}>
-                    <Typography color="textSecondary" variant="caption">
-                        Titularidade
-                    </Typography>
-                </Grid>
-                    <Scope path="ownership">
-                        <Grid item xs={6}>
-                            <Input required label="Nome" name="name" />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Input label="Apelido" name="nickname" />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Input required label="Data de nascimento" name="birthdate" />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Input label="Gênero" name="genre"/>
-                        </Grid>
-                    </Scope>
-                <Grid item xs={12}>
-                    <Typography color="textSecondary" variant="caption">
-                        Endereço
-                    </Typography>
-                </Grid>
-                    <Scope path="address">
-                        <Grid item xs={4}>
-                            <Input required label="CEP" name="zipcode" />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Input required label="Estado" name="state" />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Input required label="Cidade" name="city" />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Input required label="Número" name="number" />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Input required label="Logradouro" name="street" />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Input required label="Bairro" name="neighborhood" />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Input label="complemento" name="complement" />
-                        </Grid>
-                    </Scope>
-                    <Grid item>
-                        <Button variant="contained" color="primary" type="submit">Salvar</Button>
-                    </Grid>
+  return (
+    <Fragment>
+      <Header title={title} button={button} />
+      <Form
+        style={{ maxWidth: 1000, margin: '0 auto', marginTop: 50 }}
+        onSubmit={handleSubmit}
+        initialData={data}
+      >
+        <Grid container justify="center" spacing={16}>
+          <Grid item xs={12}>
+            <Typography color="textSecondary" variant="caption">
+              Autenticação
+            </Typography>
+          </Grid>
+          <Scope path="authentication">
+            <Grid item xs={12}>
+              <div className="form-group">
+                <Input
+                  required
+                  className="form-control"
+                  label="E-mail"
+                  name="email"
+                  type="email"
+                />
+              </div>
             </Grid>
-            </Form>
-        </Fragment>
-    );
+            <Grid item xs={12}>
+              <div className="form-group">
+                <Input required label="Senha" className="form-control" name="password" type="password" />
+              </div>
+            </Grid>
+          </Scope>
+          <Grid item xs={12}>
+            <Typography color="textSecondary" variant="caption">
+              Titularidade
+            </Typography>
+          </Grid>
+          <Scope path="ownership">
+            <Grid item xs={6}>
+              <div className="form-group">
+                <Input required label="Nome" name="name" className="form-control" />
+              </div>
+            </Grid>
+            <Grid item xs={6}>
+              <div className="form-group">
+                <Input label="Apelido" name="nickname" className="form-control" />
+              </div>
+            </Grid>
+            <Grid item xs={6}>
+              <div className="form-group">
+                <Input required label="Data de nascimento" name="birthdate" className="form-control" />
+              </div>
+            </Grid>
+            <Grid item xs={6}>
+              <div className="form-group">
+                <Input label="Gênero" name="genre" className="form-control" />
+              </div>
+            </Grid>
+          </Scope>
+          <Grid item xs={12}>
+            <Typography color="textSecondary" variant="caption">
+              Endereço
+            </Typography>
+          </Grid>
+          <Scope path="address">
+            <Grid item xs={4}>
+              <div className="form-group">
+                <Input required label="CEP" name="zipcode" className="form-control" />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className="form-group">
+                <Input required label="Estado" name="state" className="form-control" />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className="form-group">
+                <Input required label="Cidade" name="city" className="form-control" />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className="form-group">
+                <Input required label="Número" name="number" className="form-control" />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className="form-group">
+                <Input required label="Logradouro" name="street" className="form-control" />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className="form-group">
+                <Input required label="Bairro" name="neighborhood" className="form-control" />
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <div className="form-group">
+                <Input label="complemento" name="complement" className="form-control" />
+              </div>
+            </Grid>
+          </Scope>
+          <Grid item>
+            <Button variant="contained" color="primary" type="submit">
+              Salvar
+            </Button>
+          </Grid>
+        </Grid>
+      </Form>
+    </Fragment>
+  );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,32 +1,139 @@
-import React, {useEffect, useState} from 'react';
+import React, {Component} from 'react';
 
-import Data from '../components/Data';
 import Header from '../components/Header';
 import api from '../services/api';
+import {
+    IconButton,
+    Menu,
+    MenuItem,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    withStyles
+} from "@material-ui/core";
+import {Link} from "react-router-dom";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const button = {
     link: '/clients/create',
     label: 'ADICIONAR',
 };
 const title = 'Clientes';
+const styles = () => ({
+    root: {
+        maxWidth: '100%',
+        margin: 20,
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700,
+    },
+});
 
-export default function Main() {
-    const [clients, setClients] = useState([]);
+class Main extends Component {
+    state = {
+      clients: [],
+        anchorEl: null,
+    };
 
-    useEffect(() => {
-       async function loadClients() {
-           const response = await api.get('/clients');
+    async loadClients() {
+        const response = await api.get('/clients');
 
-           setClients(response.data);
-       }
+        this.setState({clients: response.data});
+    }
 
-       loadClients()
-    }, []);
+    handleClick = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
 
-    return (
-        <>
-            <Header title={title} button={button}/>
-            <Data items={clients} />
-        </>
-    );
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
+    removeItem = async (id) => {
+        this.handleClose();
+        await api.delete(`clients/${id}`);
+        this.loadClients();
+    };
+
+    componentDidMount() {
+        this.loadClients();
+    }
+
+    render() {
+        const { anchorEl } = this.state;
+        const open = Boolean(anchorEl);
+
+        return (
+            <>
+                <Header title={title} button={button}/>
+                <Paper className={this.props.classes.root}>
+                    <Table className={this.props.classes.table}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Nome</TableCell>
+                                <TableCell>Apelido</TableCell>
+                                <TableCell>E-mail</TableCell>
+                                <TableCell>Ações</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.state.clients.map(row => (
+                                <TableRow key={row._id}>
+                                    <TableCell component="th" scope="row">
+                                        {row.ownership.name}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row">
+                                        {row.ownership.nickname}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row">
+                                        {row.authentication.email}
+                                    </TableCell>
+                                    <TableCell>
+                                        <div>
+                                            <IconButton
+                                                aria-label="More"
+                                                aria-owns={open ? 'long-menu' : undefined}
+                                                aria-haspopup="true"
+                                                onClick={event => {
+                                                    event.preventDefault();
+                                                    this.handleClick(event);
+                                                }}
+                                            >
+                                                <MoreVertIcon />
+                                            </IconButton>
+                                            <Menu
+                                                id="long-menu"
+                                                anchorEl={anchorEl}
+                                                open={open}
+                                                onClose={event => {
+                                                    event.preventDefault();
+                                                    this.handleClose();
+                                                }}
+                                            >
+                                                <MenuItem component={Link} to={`/clients/${row._id}/edit`}>
+                                                    Editar
+                                                </MenuItem>
+                                                <MenuItem onClick={event => {
+                                                    event.preventDefault();
+                                                    this.removeItem(row._id);
+                                                }}>
+                                                    Deletar
+                                                </MenuItem>
+                                            </Menu>
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Paper>
+            </>
+        );
+    }
 }
+
+export default withStyles(styles)(Main);
